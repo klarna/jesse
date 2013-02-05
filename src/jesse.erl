0103cd52357983de27b3a0bbc17fb7ec666d7bf9
Alexander Dergachev <alexander.dergachev@klarna.com>
2013-02-05 14:29:10 +0100

    Update copyright in source files header

--- src/jesse.erl
+++ src/jesse.erl
@@ -11,7 +11,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%
-%% @copyright 2012 Klarna AB
+%% @copyright 2013 Klarna AB
 %% @author Alexander Dergachev <alexander.dergachev@klarna.com>
 %%
 %% @doc JESSE (JSon Schema Erlang)
