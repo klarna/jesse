b48ba5fb95b52b9cf931497c2b1543c14526a870
Stefan Strigler <stefan.strigler@1und1.de>
2014-11-14 15:17:49 +0100

    fix validating array if allowed_errors not 0

--- src/jesse_validator_draft3.erl
+++ src/jesse_validator_draft3.erl
@@ -279,11 +279,11 @@ check_union_type(Value, UnionType, State) ->
                    case jesse_lib:is_json_object(Type) of
                      true  ->
                        %% case when there's a schema in the array,
-                       %% then we need to validate against
-                       %% that schema
+                       %% then we need to validate against that schema
+                       NewState = jesse_state:set_allowed_errors(State, 0),
                        _ = jesse_schema_validator:validate_with_state( Type
                                                                      , Value
-                                                                     , State
+                                                                     , NewState
                                                                      ),
                        true;
                      false ->
