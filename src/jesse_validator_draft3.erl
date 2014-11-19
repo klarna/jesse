c22c249f8f3a7b8228c27d73c42c72e56fa97bb5
Stefan Strigler <stefan.strigler@1und1.de>
2014-11-19 11:36:27 +0100

    check_union_type: set current schema before validating particular type

--- src/jesse_validator_draft3.erl
+++ src/jesse_validator_draft3.erl
@@ -280,7 +280,8 @@ check_union_type(Value, UnionType, State) ->
                      true  ->
                        %% case when there's a schema in the array,
                        %% then we need to validate against that schema
-                       NewState = jesse_state:set_allowed_errors(State, 0),
+                       NewState0 = jesse_state:set_allowed_errors(State, 0),
+                       NewState  = jesse_state:set_current_schema(NewState0, Type),
                        _ = jesse_schema_validator:validate_with_state( Type
                                                                      , Value
                                                                      , NewState
